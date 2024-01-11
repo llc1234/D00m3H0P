e311ebabcc53640a7dc074587979934f2cf771dc
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-01-11 15:46:57 +0100

    Update ProxyTrail-PR0XY7R41L.py

--- ProxyTrail-PR0XY7R41L/ProxyTrail-PR0XY7R41L.py
+++ ProxyTrail-PR0XY7R41L/ProxyTrail-PR0XY7R41L.py
@@ -14,7 +14,7 @@ colorama.init()
 programs = [
     ["TCP_server",     "version 1.0.0"],
     ["TCP_client",     "version 1.0.0"],
-    ["rat_maker",      "version 0.1.0"],
+    ["rat_maker",      "version 0.7.0"],
     ["backdoor_maker", "version 0.1.0"]
 ]
 
